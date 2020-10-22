59b3859fbbd6ee86517ae57fe78bb4773c3fee69
Jakob Tottrup <jakobtottrup@gmail.com>
2020-10-22 22:35:54 +0200

    Finished tutorial and added firestore integration

---
+++ js/db.js
@@ -0,0 +1,51 @@
+// offline data
+db.enablePersistence()
+    .catch(err => {
+        if (err.code == 'failed-precondition') {
+            //probably multiple open tabs
+            console.log('persistence failed');
+        } else if (err.code == 'unimplemented') {
+            //lack of browser support
+            console.log('persistence is not available');
+        }
+    });
+
+// real-time listener
+db.collection('recipes').onSnapshot((snapshot) => {
+    //console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+        console.log(change, change.doc.data(), change.doc.id);
+        if (change.type === 'added') {
+            // add document data to web page
+            renderRecipe(change.doc.data(), change.doc.id);
+        }
+        if (change.type === 'removed') {
+            // remove document data from web page
+            removeRecipe(change.doc.id)
+        }
+    });
+});
+
+// add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+    };
+    db.collection('recipes').add(recipe).catch(err => console.log(err));
+    form.title.value = '';
+    form.ingredients.value = '';
+});
+
+// remove recipe
+const recipeContainer = document.querySelector('.recipes');
+recipes.addEventListener('click', evt => {
+    //console.log(evt);
+    if (evt.target.tagName === 'I') {
+        const id = evt.target.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete();
+    }
+});
