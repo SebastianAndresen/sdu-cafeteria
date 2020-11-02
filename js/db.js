a8d0a4ceef79749c420056ceee02c4201ed61cec
SebastianAndresen <sebastian.sommersted@live.dk>
2020-11-02 13:37:43 +0100

    Food items can be upvoted and downvoted

--- js/db.js
+++ js/db.js
@@ -26,14 +26,53 @@ db.collection('recipes').onSnapshot((snapshot) => {
     });
 });
 
+  /** ********** **/
+ /** FOOD ITEMS **/
+/** ********** **/
+
+const foodItemCollection = 'fooditems';
+
 // food-items real-time listener
-db.collection('fooditems').onSnapshot(snapshot => {
+db.collection(foodItemCollection).onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
             renderFoodItem(change.doc.data(), change.doc.id);
+        }
+        if (change.type === 'modified') {
+            modifyFoodItem(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
             removeFoodItem(change.doc.id);
         }
     });
 });
+
+const clickUpvote = (itemid, isUpvoted) => {
+    if (isUpvoted) {
+        db.collection(foodItemCollection).doc(itemid).update({
+            user_upvotes: firebase.firestore.FieldValue.arrayRemove(user)
+        });
+    } else {
+        db.collection(foodItemCollection).doc(itemid).update({
+            user_upvotes: firebase.firestore.FieldValue.arrayUnion(user),
+            user_downvotes: firebase.firestore.FieldValue.arrayRemove(user)
+        });
+    }
+}
+
+const clickDownvote = (itemid, isDownvoted) => {
+    if (isDownvoted) {
+        db.collection(foodItemCollection).doc(itemid).update({
+            user_downvotes: firebase.firestore.FieldValue.arrayRemove(user)
+        });
+    } else {
+        db.collection(foodItemCollection).doc(itemid).update({
+            user_downvotes: firebase.firestore.FieldValue.arrayUnion(user),
+            user_upvotes: firebase.firestore.FieldValue.arrayRemove(user)
+        });
+    }
+}
+
+const clickFavorite = (itemid, isFavorite) => {
+    console.log(`USer ${user} favorites ${itemid} (DB)`);
+}
--- js/sections/menu.js
+++ js/sections/menu.js
@@ -1,4 +1,6 @@
-console.log("MENU SCRIPT LOADED v11");
+console.log("MENU SCRIPT LOADED v18");
+
+const fooditems = document.querySelector('#food_items');
 
 const foodcategoriesDOM = [
     document.querySelector('#food_category_news'),
@@ -7,23 +9,60 @@ const foodcategoriesDOM = [
 ];
 
 const renderFoodItem = (data, id) => {
-    console.log("RENDER FOOD ITEM");
-    console.log(data);
+    if (!data.visible) return;
+
+    const isUpvoted = data.user_upvotes.includes(user);
+    const isDownvoted = data.user_downvotes.includes(user);
+    const isFavorite = false;
+
     const html = `
-        <div class="card-panel recipe white row" data-id="${id}">
+        <div class="fooditem card-panel white row" data-id="${id}">
             <img src="${data.image}" alt="${data.title} image">
-            <div class="recipe-details">
-                <div class="food-title">${data.title}</div>
-                <div class="food-">${data.ingredients}</div>
-            </div>
-            <div class="recipe-delete">
-                    <i class="material-icons" data-id="${id}">delete_outline</i>
-            </div>
+            <div class="food-title">${data.title}</div>
+            <div class="food-diets">${data.diets}</div>
+            <div class="food-allergies">${data.allergies}</div>
+            <div class="food-price">${data.price}</div>
+
+            <div class="food-upvotes">Upvotes: ${data.user_upvotes.length}</div>
+            <div class="food-downvotes">Downvotes: ${data.user_downvotes.length}</div>
+
+            <i class="btn_upvote material-icons" data-id="${id}" data-upvoted="${isUpvoted}" style="color: ${isUpvoted ? 'green' : 'gray'}">arrow_circle_up</i>
+            <i class="btn_downvote material-icons" data-id="${id}" data-downvoted="${isDownvoted}" style="color: ${isDownvoted ? 'red' : 'gray'}">arrow_circle_down</i>
+            <i class="btn_favorite material-icons" data-id="${id}" data-favorite="${isFavorite}" style="color: ${isFavorite ? 'yellow' : 'gray'}">star_rate</i>
         </div>
     `;
 
     if (data.category < foodcategoriesDOM.length);
     foodcategoriesDOM[data.category].innerHTML += html;
+}
+
+fooditems.addEventListener('click', evt => {
+    if (evt.target.tagName === 'I') {
+        const id = evt.target.getAttribute('data-id');
+        switch (evt.target.className.split(" ")[0]) {
+            case 'btn_upvote': 
+                clickUpvote(id,
+                    evt.target.getAttribute('data-upvoted') == 'true'
+                    );
+                break;
+            case 'btn_downvote':
+                clickDownvote(id,
+                    evt.target.getAttribute('data-downvoted') == 'true'
+                    );
+                break;
+            case 'btn_favorite':
+                clickFavorite(id,
+                    evt.target.getAttribute('data-favorite') == 'true'
+                    );
+                break;
+            default:
+                console.log(`Unknown class: ${evt.target.className.split(" ")[0]}`);
+        }
+    }
+});
+
+const modifyFoodItem = () => {
+    console.log("MODIFY FOOD ITEM");
 }
 
 const removeFoodItem = () => {
