d0cd7a05ad3f849a27b4436f523157b22af6a370
morob <morob05@student.sdu.dk>
2020-11-20 22:48:13 +0100

    Rettet event-listener til kun at lytte på input

--- js/sections/filters.js
+++ js/sections/filters.js
@@ -3,62 +3,64 @@ console.log("FILTERS SCRIPT LOADED v1");
 const filters = document.querySelector('#filters');
 
 filters.addEventListener('click', evt => {
-    const filterVegan = firebase.app().functions('europe-west1').httpsCallable('EU_filterVegan');
-    const filterVegetarian = firebase.app().functions('europe-west1').httpsCallable('EU_filterVegetarian');
-    const filterHalal = firebase.app().functions('europe-west1').httpsCallable('EU_filterHalal');
-    const filterShellFish = firebase.app().functions('europe-west1').httpsCallable('EU_filterNuts');
-    const filterNuts = firebase.app().functions('europe-west1').httpsCallable('EU_filterShellFish');
+    if(evt.target.tagName=== 'INPUT'){
+        const filterVegan = firebase.app().functions('europe-west1').httpsCallable('filterVegan');
+        const filterVegetarian = firebase.app().functions('europe-west1').httpsCallable('filterVegetarian');
+        const filterHalal = firebase.app().functions('europe-west1').httpsCallable('filterHalal');
+        const filterShellFish = firebase.app().functions('europe-west1').httpsCallable('filterNuts');
+        const filterNuts = firebase.app().functions('europe-west1').httpsCallable('filterShellFish');
 
-    const id = evt.target.getAttribute('data-id');
-    console.log(id);
-    console.log(user);
-    switch(evt.target.className.split(" ")[0]){
-        case 'vegan':
-            filterVegan({
-                id,
-                filteredVegan
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'vegetarian':
-            filterVegetarian({
-                id,
-                filteredVegetarian
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'halal':
-            filterHalal({
-                id,
-                filteredHalal
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'shell-fish':
-            filterShellFish({
-                id,
-                filteredShellFish
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'nuts':
-            filterNuts({
-                id,
-                filteredNuts
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        default:
-            break;
+        const id = evt.target.getAttribute('data-id');
+        console.log(id);
+        console.log(user);
+        switch(evt.target.className.split(" ")[0]){
+            case 'vegan':
+                filterVegan({
+                    id,
+                    filterVegan
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'vegetarian':
+                filterVegetarian({
+                    id,
+                    filterVegetarian
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'halal':
+                filterHalal({
+                    id,
+                    filterHalal
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'shell-fish':
+                filterShellFish({
+                    id,
+                    filterShellFish
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'nuts':
+                filterNuts({
+                    id,
+                    filteredNuts
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            default:
+                break;
+        }
     }
 });
--- js/sections/notifications.js
+++ js/sections/notifications.js
@@ -3,42 +3,45 @@ console.log("NOTIFICATIONS SCRIPT LOADED v1");
 const notifications = document.querySelector('#notifications');
 
 notifications.addEventListener('click', evt => {
-    const notifyVegan = firebase.app().functions('europe-west1').httpsCallable('EU_notifyVegan');
-    const notifyVegetarian = firebase.app().functions('europe-west1').httpsCallable('EU_notifyVegetarian');
-    const notifyFavorites = firebase.app().functions('europe-west1').httpsCallable('EU_notifyFavorites');
+    if(evt.target.tagName=== 'INPUT'){
 
-    const id = evt.target.getAttribute('data-id');
-    console.log(id);
-    console.log(user);
-    switch(evt.target.className.split(" ")[0]){
-        case 'vegan':
-            notifyVegan({
-                id,
-                notifiedVegan
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'vegetarian':
-            notifyVegetarian({
-                id,
-                notifiedVegetarian
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        case 'favorites':
-            notifyFavorites({
-                id,
-                notifiedFavorites
-            }).catch(err => {
-                //TODO: make nice and shiny error message for user
-                console.log('ERROR: ', err.message);
-            });
-            break;
-        default:
-            break;
+        const notifyVegan = firebase.app().functions('europe-west1').httpsCallable('notifyVegan');
+        const notifyVegetarian = firebase.app().functions('europe-west1').httpsCallable('notifyVegetarian');
+        const notifyFavorites = firebase.app().functions('europe-west1').httpsCallable('notifyFavorites');
+
+        const id = evt.target.getAttribute('data-id');
+        console.log(id);
+        console.log(user);
+        switch(evt.target.className.split(" ")[0]){
+            case 'vegan':
+                notifyVegan({
+                    id,
+                    notifyVegan
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'vegetarian':
+                notifyVegetarian({
+                    id,
+                    notifyVegetarian
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            case 'favorites':
+                notifyFavorites({
+                    id,
+                    notifyFavorites
+                }).catch(err => {
+                    //TODO: make nice and shiny error message for user
+                    console.log('ERROR: ', err.message);
+                });
+                break;
+            default:
+                break;
+        }
     }
 });
