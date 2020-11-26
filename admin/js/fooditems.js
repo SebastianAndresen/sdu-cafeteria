200187a16ce98b8b9c46e89a1cac6fa89cdde43c
SebastianAndresen <sebastian.sommersted@live.dk>
2020-11-26 16:57:02 +0100

    styled food items on admin page

--- admin/js/fooditems.js
+++ admin/js/fooditems.js
@@ -9,20 +9,21 @@ const renderFoodItem = (data, id) => {
                     <td rowspan="3" colspan="3" class="fooditemImage">
                         <img src="${data.image}" alt="${data.title} image">
                     </td>
-                    <td>${data.title}</td>
-                    <td>${data.diets}</td>
-                    <td class="floatright">
+                    <td><p class="desc">Title:</p><h3>${data.title}</h3></td>
+                    <td><p class="desc">Diets:</p><p>${data.diets}</p></td>
+                    <td>
                         <button type="button" onclick="editfooditem('${id}')">Edit</button>
+                        <p class="subinfo">${'latest edit'}</p>
                     </td>
                 </tr>
                 <tr>
-                    <td>${data.price}</td>
-                    <td>${data.allergies}</td>
-                    <td class="floatright">latest edit</td>
+                    <td><p class="desc">Price:</p><p>${data.price}</p></td>
+                    <td><p class="desc">Allergies</p><p>${data.allergies}</p></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td colspan="2">Visibility status</td>
-                    <td class="floatright">
+                    <td colspan="2"><p class="subinfo">Visibility status</p></td>
+                    <td>
                         <button type="button" onclick="showhide('${id}')">Show/Hide</button>
                     </td>
                 </tr>
@@ -30,8 +31,8 @@ const renderFoodItem = (data, id) => {
                     <td class="fooditemUpvote">${data.user_upvotes.length} <i class="material-icons">arrow_circle_up</i></td>
                     <td class="fooditemDownvote">${data.user_downvotes.length} <i class="material-icons">arrow_circle_down</i></td>
                     <td class="fooditemFavorite">${data.user_favorites.length} <i class="material-icons">star_rate</i></td>
-                    <td colspan="2">Score reset status</td>
-                    <td class="floatright">
+                    <td colspan="2"><p class="subinfo">Score reset status</p></td>
+                    <td>
                         <button type="button" onclick="resetscore('${id}')">Reset Score</button>
                     </td>
                 </tr>
