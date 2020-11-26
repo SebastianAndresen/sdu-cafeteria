91cab5be2aabec87dc259eda5d81b73a8f0f686e
SebastianAndresen <sebastian.sommersted@live.dk>
2020-11-26 23:47:33 +0100

    created nice checkboxes for diets and allergies in the admin editor

--- admin/js/categories.js
+++ admin/js/categories.js
@@ -1,12 +1,19 @@
 
 const sidebar = document.getElementById('food_sidebar');
-const btns = sidebar.getElementsByTagName('p');
+const sidebar_btns = sidebar.getElementsByTagName('p');
 
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
+for (let i = 0; i < sidebar_btns.length; i++) {
+    sidebar_btns[i].addEventListener("click", function () {
         let current = sidebar.getElementsByClassName('active');
         current[0].className = current[0].className.replace('active', '');
         this.className += ' active';
         console.log(this.getAttribute('data-cat'));
     });
 }
+
+const editor_checkbox = document.querySelector('ul');
+editor_checkbox.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
