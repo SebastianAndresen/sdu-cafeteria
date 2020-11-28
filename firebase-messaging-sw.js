fd508f2ff18bc3b725a6bce42c4369032ada9278
jakobtottrup <jakobtottrup@gmail.com>
2020-11-28 09:40:42 +0100

    worked on push notifications

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -18,7 +18,7 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
-    const notificationTitle = 'Background Message Title';
+    const notificationTitle = 'Background Message';
     const notificationOptions = {
         body: 'Background Message body.',
         icon: '/firebase-logo.png'
