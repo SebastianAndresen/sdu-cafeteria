d55fc53a4807e7ffb412aec5deca32c79bf6d20e
Jakob Tottrup <jakobtottrup@gmail.com>
2020-12-04 10:15:21 +0100

    Fixed CORS issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let sharedVersionNum = 231;
+let sharedVersionNum = 232;
 const dynamicCache = `site-dynamic-v1.3.${sharedVersionNum}`;
 const dynamicCache_MAXSIZE = 15;
 const staticCache = `site-static-v1.3.${sharedVersionNum}`; //Increment whenever a change is made to one of the following assets
@@ -15,11 +15,6 @@ const assets = [
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://code.jquery.com/jquery-3.5.1.min.js',
-    'https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js',
-    'https://www.gstatic.com/firebasejs/8.1.1/firebase-firestore.js',
-    'https://www.gstatic.com/firebasejs/8.1.1/firebase-functions.js',
-    'https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js',
-    'https://www.gstatic.com/firebasejs/7.24.0/firebase-auth.js',
     '/pages/fallback.html'
 ];
 // cache size limit function
