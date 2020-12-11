96bdb7650cd9276dcbe815c343ca049777060448
SebastianAndresen <sebastian.sommersted@live.dk>
2020-12-11 09:55:20 +0100

    changed data.visibility check to new version (index instead of boolean)

--- js/sections/menu.js
+++ js/sections/menu.js
@@ -1,4 +1,3 @@
-console.log("MENU SCRIPT LOADED v18");
 
 const fooditems = document.querySelector('#food_items');
 
@@ -9,7 +8,7 @@ const foodcategoriesDOM = [
 ];
 
 const renderFoodItem = (data, id) => {
-    if (!data.visible) return;
+    if (data.visible == 0) return;
 
     const isUpvoted = data.user_upvotes.includes(user);
     const isDownvoted = data.user_downvotes.includes(user);
