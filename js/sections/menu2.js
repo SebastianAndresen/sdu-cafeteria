a4d689b73a93cbd19a082d62747799fd2cfb48d6
SebastianAndresen <sebastian.sommersted@live.dk>
2020-12-14 02:27:10 +0100

    all food data is now rendered and updated on modify

--- js/sections/menu2.js
+++ js/sections/menu2.js
@@ -14,49 +14,54 @@ const foodcategoriesDOM = [
     document.querySelector('#food_category_drink')
 ];
 
-const renderFoodItem = (data, id) => {
-    if (data.visible == 0 || data.visible == 2) return;
-
-    const isUpvoted = data.user_upvotes.includes(user);
-    const isDownvoted = data.user_downvotes.includes(user);
-    const isFavorite = data.user_favorites.includes(user);
-
-    const jsoncontent = {
+const buildFoodJSONfile = (data) => {
+    const votes = data.user_upvotes.length + data.user_downvotes.length;
+    const upvotes = data.user_upvotes.length;
+    const downvotes = data.user_downvotes.length;
+    return {
         category: data.category,
         title: data.title,
         image: data.image,
         price: data.price,
-        desc: data.desc,
         contains: data.contains,
-        upvotes: data.user_upvotes.length,
-        downvotes: data.user_downvotes.length,
-        score: ((data.user_upvotes.length + data.user_downvotes.length) > 0 ? (Math.floor(data.user_upvotes.length / (data.user_upvotes.length + data.user_downvotes.length) * 100) / 10) : '-'),
+        votes,
+        upvotes,
+        downvotes,
+        score: (votes > 0 ? (Math.floor(upvotes / (votes) * 100) / 10) : '-'),
         personal: {
             upvote: data.user_upvotes.includes(user),
             downvote: data.user_downvotes.includes(user),
             favorite: data.user_favorites.includes(user)
         }
     };
+};
+
+const renderFoodItem = (data, id) => {
+    if (data.visible == 0 || data.visible == 2) return;
+
+    const foodJSON = buildFoodJSONfile(data);
 
     const html = `
-    <div class="fooditem" data-id="ID_${id}" id="ID_${id}" data-jsoncontent='${JSON.stringify(jsoncontent)}'>
-        <img src="${data.image}" alt="${data.title} image">
-        <div>
-            <div class="food-title card-data">${data.title}</div>
-            <div class="food-price card-data">${data.price}</div>
-            <div class="food-ingredients card-data">${containsIntArrToStringArr(data.contains)}</div>
-            
-            <div class="food-upvotes card-data">Upvotes: ${data.user_upvotes.length}</div>
-            <div class="food-downvotes card-data">Downvotes: ${data.user_downvotes.length}</div>
-            
-            <i class="btn_upvote card-data material-icons${isUpvoted ? ' active' : ''}" data-id="${id}" data-upvoted="${isUpvoted}">arrow_circle_up</i>
-            <i class="btn_downvote card-data material-icons${isDownvoted ? ' active' : ''}" data-id="${id}" data-downvoted="${isDownvoted}">arrow_circle_down</i>
-            <i class="btn_favorite card-data material-icons${isFavorite ? ' active' : ''}" data-id="${id}" data-favorite="${isFavorite}">star_rate</i>
-        </div>
+    <div class="fooditem" data-id="ID_${id}" id="ID_${id}" data-jsoncontent='${JSON.stringify(foodJSON)}'>
+        <img src="${foodJSON.image}" alt="${foodJSON.title} image">
+
+        <div class="food-title card-data">${foodJSON.title}</div>
+        <div class="food-price card-data">${foodJSON.price}</div>
+        <div class="food-ingredients card-data">${containsIntArrToStringArr(foodJSON.contains)}</div>
+        
+        <div class="food-upvotes card-data">Upvotes: ${foodJSON.upvotes}</div>
+        <div class="food-downvotes card-data">Downvotes: ${foodJSON.downvotes}</div>
+
+        <div class="food-votes card-data">Votes: ${foodJSON.votes}</div>
+        <div class="food-score card-data">Score: ${foodJSON.score}</div>
+        
+        <i class="btn_upvote card-data material-icons${foodJSON.personal.upvote ? ' active' : ''}" data-id="${id}" data-upvoted="${foodJSON.personal.upvote}">arrow_circle_up</i>
+        <i class="btn_downvote card-data material-icons${foodJSON.personal.downvote ? ' active' : ''}" data-id="${id}" data-downvoted="${foodJSON.personal.downvote}">arrow_circle_down</i>
+        <i class="btn_favorite card-data material-icons${foodJSON.personal.favorite ? ' active' : ''}" data-id="${id}" data-favorite="${foodJSON.personal.favorite}">star_rate</i>
     </div>
     `;
 
-    if (isFavorite)
+    if (foodJSON.personal.favorite)
         renderFavoriteItem(data, id);
 
     if (data.category < foodcategoriesDOM.length)
@@ -135,42 +140,39 @@ const containsIntArrToStringArr = (int_arr) => {
 }
 
 const modifyFoodItem = (data, id) => {
-    if (!data.visible) return;
+    if (data.visible == 0 || data.visible == 2) {
+        // TODO: REMOVE FOODITEM IF IT IS VISIBLE ALREADY
+        return;
+    } else {
+        // TODO: ADD FOODITEM IF IT IS NOT VISIBLE ALREADY
+    }
+
+    const foodJSON = buildFoodJSONfile(data);
 
     const fooditem = document.querySelector(`.fooditem[data-id=ID_${id}]`);
-    // TODO - update image
-    fooditem.querySelector('.food-title').innerHTML = data.title;
-    fooditem.querySelector('.food-ingredients').innerHTML = containsIntArrToStringArr(data.contains);
-    fooditem.querySelector('.food-price').innerHTML = data.price;
-    fooditem.querySelector('.food-upvotes').innerHTML = `Upvotes: ${data.user_upvotes.length}`;
-    fooditem.querySelector('.food-downvotes').innerHTML = `Downvotes: ${data.user_downvotes.length}`;
-
-    const isUpvoted = data.user_upvotes.includes(user);
-    const isDownvoted = data.user_downvotes.includes(user);
-    const isFavorite = data.user_favorites.includes(user);
 
     const btnUpvote = fooditem.querySelector('.btn_upvote');
     const btnDownvote = fooditem.querySelector('.btn_downvote');
     const btnFavorite = fooditem.querySelector('.btn_favorite');
 
-    //fooditem.querySelector('.btn_upvote').setAttribute('data-upvoted', isUpvoted).classList.remove("active").classList.add(isUpvoted ? 'active':'');
+    // TODO - update image
+    fooditem.querySelector('.food-title').innerHTML = foodJSON.title;
+    fooditem.querySelector('.food-ingredients').innerHTML = containsIntArrToStringArr(foodJSON.contains);
+    fooditem.querySelector('.food-price').innerHTML = foodJSON.price;
+    fooditem.querySelector('.food-upvotes').innerHTML = `Upvotes: ${foodJSON.upvotes}`;
+    fooditem.querySelector('.food-downvotes').innerHTML = `Downvotes: ${foodJSON.downvotes}`;
+    fooditem.querySelector('.food-votes').innerHTML = `Votes: ${foodJSON.votes}`;
+    fooditem.querySelector('.food-score').innerHTML = `Score: ${foodJSON.score}`;
 
-    btnUpvote.setAttribute('data-upvoted', isUpvoted);
-    btnDownvote.setAttribute('data-downvoted', isDownvoted);
-    btnFavorite.setAttribute('data-favorite', isFavorite);
+    btnUpvote.setAttribute('data-upvoted', foodJSON.personal.upvote);
+    btnDownvote.setAttribute('data-downvoted', foodJSON.personal.downvote);
+    btnFavorite.setAttribute('data-favorite', foodJSON.personal.favorite);
 
     btnUpvote.classList.remove("active");
     btnDownvote.classList.remove("active");
     btnFavorite.classList.remove("active");
 
-    if (isUpvoted) btnUpvote.classList.add("active");
-    if (isDownvoted) btnDownvote.classList.add("active");
-    if (isFavorite) btnFavorite.classList.add("active");
-    /*
-    if (isFavorite) {
-        btnFavorite.classList.add("active");
-    }
-    else{
-        removeFavorite(id);
-    }*/
+    if (foodJSON.personal.upvote) btnUpvote.classList.add("active");
+    if (foodJSON.personal.downvote) btnDownvote.classList.add("active");
+    if (foodJSON.personal.favorite) btnFavorite.classList.add("active");
 }
